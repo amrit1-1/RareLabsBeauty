4fe3e0d292d42470587735761fae5b86d3ea82c1
Amrit <amrit26.2002@gmail.com>
2024-05-19 23:17:35 +0100

    slight footer change.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from "react-router-dom";
 
+// This element displays a footer with some of the nav links and customer service contact information.
 export default function Footer() {
     return(
         <div className="footer">
